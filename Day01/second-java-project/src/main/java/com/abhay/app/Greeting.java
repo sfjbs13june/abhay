a5766c7cf291436444f9778cd6b943e174a0bcec
abhay <abhaysingh.thakur@tcs.com>
2023-01-12 07:14:20 +0000

    added greeting.java file and added test cases for greeting.java

---
+++ Day01/second-java-project/src/main/java/com/abhay/app/Greeting.java
@@ -0,0 +1,16 @@
+package com.abhay.app;
+
+public class Greeting {
+
+    public String SayWelcome(){
+        return "welcome from greeting";
+    }
+
+    public String SayHello(){
+        return "hello from greeting";
+    }
+
+    public String SayBye(){
+        return "bye from greeting";
+    }
+}
--- Day01/second-java-project/src/test/java/com/abhay/app/AppTest.java
+++ Day01/second-java-project/src/test/java/com/abhay/app/AppTest.java
@@ -1,38 +1,37 @@
 package com.abhay.app;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    @Test
+    public void SayWelcome() {
+        String expectedResult = "welcome from greeting";
+        Greeting greeting = new Greeting();
+        String result1 = greeting.SayWelcome();
+        Assert.assertEquals(expectedResult,result1);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void SayHello() {
+        String expectedResult = "hello from greeting";
+        Greeting greeting = new Greeting();
+        String result2 = greeting.SayHello();
+        Assert.assertEquals(expectedResult,result2);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void SayBye() {
+        String expectedResult = "bye from greeting";
+        Greeting greeting = new Greeting();
+        String result3 = greeting.SayBye();
+        Assert.assertEquals(expectedResult,result3);
     }
+
 }
