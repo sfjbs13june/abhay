5eed301495581a8228a4a53e69b6040ce8666b10
sfjbs <abhaysingh.thakur@tcs.com>
2023-02-06 19:06:53 +0000

    Day18 - Mini Project.

---
+++ Day18/hospital-management-app/src/main/java/com/abhay/app/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.abhay.app.controller;
+
+
+import com.abhay.app.model.Appointment;
+import com.abhay.app.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+
+    @Autowired
+    AppointmentRepository appointmentRepository;
+    @RequestMapping(value = "/myappointment",method = RequestMethod.GET)
+    public Appointment getMyAppointments(@RequestParam("patientName") String patientName)
+    {
+        return appointmentRepository.findByPatientName(patientName);
+    }
+
+    @RequestMapping(value = "/Save",method = RequestMethod.POST)
+    public Appointment saveAppointment(@RequestBody Appointment appointment)
+    {
+        appointment = appointmentRepository.save(appointment);
+        return appointment;
+    }
+}
---
+++ Day18/hospital-management-app/src/main/java/com/abhay/app/repository/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package com.abhay.app.repository;
+
+import com.abhay.app.model.Appointment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends MongoRepository<Appointment, String> {
+
+    public Appointment save(Appointment ap);
+    public Appointment findBydoctorName(String doctorName);
+    public Appointment findByPatientName(String patientName);
+}
