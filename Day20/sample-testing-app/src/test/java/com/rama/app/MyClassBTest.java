62176219de36e54e06f201953d3f6ce1b7342e69
sfjbs <abhaysingh.thakur@tcs.com>
2023-02-08 07:04:05 +0000

    Day20 - added test cases

---
+++ Day20/sample-testing-app/src/test/java/com/rama/app/MyClassBTest.java
@@ -0,0 +1,56 @@
+package com.rama.app;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MyClassBTest {
+    @InjectMocks
+    MyclassB myclassB;
+
+
+    @Mock
+    MyClassC myClassC;
+
+    @BeforeEach
+    void setUp(){
+        myClassC = Mockito.mock(MyClassC.class);
+    }
+
+    @Test
+    public void getResultTest(){
+        String result = myclassB.getResult();
+        assertEquals("my result",result);
+    }
+
+    @Test
+    public void getMyStringTest(){
+        //when(myclassB.getMyString(anyString())).thenReturn("my result my data");
+        String result = myclassB.getMyString("my data");
+        assertEquals("my result my data",result);
+    }
+
+    @Test
+    public void getClassCResultTest(){
+        when(myClassC.getMyAge(anyInt())).thenReturn(45);
+        int age = myclassB.getClassCRestult(myClassC);
+        assertEquals(45,age);
+    }
+
+
+    @Test
+    public void noreturnDataTst(){
+        myclassB.NoreturnData("my data");
+        String result = "my data";
+        assertEquals("my data" , result);
+
+    }
+}
